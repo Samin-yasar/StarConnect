504b04681102a8231ea8c74b92ce3d33c7cf2155
Samin Yasar <79013179+Samin-yasar@users.noreply.github.com>
2025-04-12 14:55:26 +0600

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,25 +1,54 @@
-function addMessage(content, isOwnMessage, nickname = 'Anonymous') {
-  const messages = document.getElementById('messages');
-  const msg = document.createElement('div');
-  msg.classList.add('msg');
-  if (isOwnMessage) msg.classList.add('me');
+(function () {
+  function addMessage(content, isOwnMessage, nickname = 'Anonymous') {
+    const messages = document.getElementById('messages');
+    if (!messages) {
+      console.error('Messages container not found');
+      return;
+    }
 
-  const nick = document.createElement('span');
-  nick.className = 'nickname';
-  nick.textContent = nickname;
-  msg.appendChild(nick);
+    const msg = document.createElement('div');
+    msg.classList.add('msg');
+    if (isOwnMessage) msg.classList.add('me');
 
-  const time = document.createElement('span');
-  time.className = 'timestamp';
-  time.textContent = new Date().toLocaleTimeString();
-  msg.appendChild(time);
+    const nick = document.createElement('span');
+    nick.className = 'nickname';
+    nick.textContent = nickname.replace(/[<>&"']/g, (m) => ({
+      '<': '&lt;',
+      '>': '&gt;',
+      '&': '&amp;',
+      '"': '&quot;',
+      "'": '&apos;'
+    }[m]));
+    msg.appendChild(nick);
 
-  if (typeof content === 'string') {
-    msg.appendChild(document.createTextNode(content));
-  } else {
-    msg.appendChild(content);
+    const time = document.createElement('span');
+    time.className = 'timestamp';
+    time.textContent = new Date().toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit'
+    });
+    msg.appendChild(time);
+
+    const contentDiv = document.createElement('div');
+    contentDiv.className = 'content';
+    if (typeof content === 'string') {
+      contentDiv.textContent = content.replace(/[<>&"']/g, (m) => ({
+        '<': '&lt;',
+        '>': '&gt;',
+        '&': '&amp;',
+        '"': '&quot;',
+        "'": '&apos;'
+      }[m]));
+    } else {
+      contentDiv.appendChild(content);
+    }
+    msg.appendChild(contentDiv);
+
+    messages.appendChild(msg);
+    messages.scrollTo({ top: messages.scrollHeight, behavior: 'smooth' });
   }
 
-  messages.appendChild(msg);
-  messages.scrollTop = messages.scrollHeight;
-}
+  window.uiModule = {
+    addMessage
+  };
+})();
